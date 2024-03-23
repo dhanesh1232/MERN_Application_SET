2ec8ddae572cb32691d35d0c456a4636f4d7944e
dhanesh1232 <dhaneshreddy980@gmail.com>
2024-03-23 14:07:28 +0530

    get method added

--- Server/server.js
+++ Server/server.js
@@ -4,3 +4,7 @@ const app = express();
 app.listen(3002, () => {
   console.log("Server running on port 3002 \nhttp://localhost:3002");
 });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!", success: true });
+});
